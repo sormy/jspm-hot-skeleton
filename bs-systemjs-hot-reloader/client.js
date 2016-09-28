eac4469b029f7ca5a9e39a8f7e5052267df40f07
Artem Butusov <artem.butusov@unvsoft.com>
2016-09-27 21:58:37 -0400

    bs-systemjs-hot-reloader with css support

--- bs-systemjs-hot-reloader/client.js
+++ bs-systemjs-hot-reloader/client.js
@@ -4,10 +4,18 @@
   socket.on('system:reload', function (data) {
     var loader = SystemJS || System;
 
+    if (!loader) {
+      throw 'BrowserSync SystemJS Hot Reloader: unable to find SystemJS';
+    }
+
     loader.import('systemjs-hot-reloader')
       .then(function (exports) {
         var reloader = exports.default;
         reloader.reloadPath(data.path);
+      })
+      .catch(function (error) {
+        console.error('BrowserSync SystemJS Hot Reloader: unable find SystemJS Hot Reloader');
+        console.error(error);
       });
   });
 })(window, window.___browserSync___);
